7e1fc94e6d61e16c59f96f4bca5fe40613140ad1
Wes Luntsford <wesleyluntsford@gmail.com>
2022-08-25 17:21:30 -0500

    Initial Commit

---
+++ StopLight/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StopLight
+//
+//  Created by Wesley Luntsford on 8/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StopLight/StopLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopLightApp.swift
+//  StopLight
+//
+//  Created by Wesley Luntsford on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct StopLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
